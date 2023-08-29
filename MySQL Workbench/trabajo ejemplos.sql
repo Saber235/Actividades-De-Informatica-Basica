7973ab39c2337f9ec901353a6de7db7d715cf99c
Jhonny_Torres <128723713+Saber235@users.noreply.github.com>
2023-08-29 10:14:30 -0500

    en proseso 2.0

--- MySQL Workbench/trabajo ejemplos.sql
+++ MySQL Workbench/trabajo ejemplos.sql
@@ -23,20 +23,50 @@ select * from tb_posibles_padres;
 
 CREATE TABLE tb_posibles_hijos(	
 	dni_hj varchar(15)   not null,
+    dni_pp varchar(15)   not null,
+    dni_pm  varchar(15)   not null,
 	nombres varchar(50)  not null,
 	apellidos varchar(50) not null,
 	fecha_nac date      not null,
 	edad  int              null, 
 	 primary key(dni_hj)
 );
+DROP table  tb_posibles_hijos;
 
-drop table tb_posibles_hijo
 describe tb_posibles_hijos;
 select * from tb_posibles_hijos;
+
+
 select * from tb_posibles_padres,tb_posibles_hijos
-where tb_posibles_padres.dni-pp = tb_posibles_hijos.dni-pp
+where tb_posibles_padres.dni_pp = tb_posibles_hijos.dni_pp;
 
+create table tb_posibles_nietos(
+	dni_nt varchar(15)   not null,
+    dni_hj  varchar(15)   not null,
+	nombres varchar(50)  not null,
+	apellidos varchar(50) not null,
+	fecha_nac date      not null,
+	edad  int              null, 
+	 primary key(dni_nt)
+);
+DROP table tb_posibles_nietos;
+describe tb_posibles_nietos;
+select * from tb_posibles_nietos;
 
+select * from tb_posibles_hijos,tb_posibles_nietos,tb_posibles_nietos
+where tb_posibles_hijos.dni-hj = tb_posibles_nietos.dni_hj;
 
+create table tb_posibles_madres(
+	dni_pm varchar(15)   not null,
+    nombres varchar(50)  not null,
+	apellidos varchar(50) not null,
+    fecha_nac date      not null,
+     primary key(dni_pm)
+);
+ 
+ drop table tb_posibles_madres;
+ describe  tb_posibles_madres;
+ select * from  tb_posibles_madres;
+ 
 
 
